4dfae6454f9d20073733ef2bc8b3c04fb4b33ac5
Mike Smith <smith@oneironautics.org>
2013-09-25 18:41:53 -0500

    Tabs are now spaces for consistency.

--- Shapefile/Shapefile.c
+++ Shapefile/Shapefile.c
@@ -35,15 +35,15 @@ const char* g_path;
 SFPolygon* g_polygons;
 int32_t g_polygons_index;
 
-/*	int32_t byteswap32(int32_t value)
+/*    int32_t byteswap32(int32_t value)
 
 A cross-platform helper for flipping endianess.
 
 Arguments:
-	value: the un-byteswapped value.
+    value: the un-byteswapped value.
 
 Returns:
-	int32_t: the byteswapped value.
+    int32_t: the byteswapped value.
 
 */
 int32_t byteswap32(int32_t value)
@@ -57,44 +57,44 @@ int32_t byteswap32(int32_t value)
     return value;
 }
 
-/*	void print_msg(const char* format, ...)
+/*    void print_msg(const char* format, ...)
 
 A cross-platform helper for sprintf to use sprintf_s on Windows platforms.
 
 Arguments:
-	format: the printf-esque format string.
-	...: varargs of the printf formatters.
+    format: the printf-esque format string.
+    ...: varargs of the printf formatters.
 
 Returns:
-	N/A.
+    N/A.
 
 */
 void print_msg(const char* format, ...)
 {
-	char buf[1024];
+    char buf[1024];
 
-	va_list args;
-	va_start(args, format);
-	
+    va_list args;
+    va_start(args, format);
+    
 #ifndef _WIN32
-	sprintf(buf, format, args);
+    sprintf(buf, format, args);
 #else
-	sprintf_s(buf, 1024, format, args);
+    sprintf_s(buf, 1024, format, args);
 #endif
 
-	va_end(args);
+    va_end(args);
 }
 
-/*	SFResult open_shapefile(const char* path)
+/*    SFResult open_shapefile(const char* path)
 
 Opens a shapefile for reading.
 
 Arguments:
-	const char* path: the path to the shapefile to open.
-	
+    const char* path: the path to the shapefile to open.
+    
 Returns:
-	SF_ERROR: the shapefile could not be opened.
-	SF_OK: the shapefile could be opened.
+    SF_ERROR: the shapefile could not be opened.
+    SF_OK: the shapefile could be opened.
 */
 SFResult open_shapefile(const char* path)
 {
@@ -103,7 +103,7 @@ SFResult open_shapefile(const char* path)
 #ifndef _WIN32
     g_shapefile = fopen(g_path, "rb");
 #else
-	fopen_s(&g_shapefile, g_path, "rb");
+    fopen_s(&g_shapefile, g_path, "rb");
 #endif
     
     if ( g_shapefile == 0 ) {
@@ -114,21 +114,21 @@ SFResult open_shapefile(const char* path)
     return SF_OK;
 }
 
-/*	SFResult validate_shapefile(void)
+/*    SFResult validate_shapefile(void)
 
 Validates that a file is an ESRI shapefile based on the header.
 
 Arguments:
-	N/A.
-	
+    N/A.
+    
 Returns:
-	SF_ERROR: the file was not a valid ESRI shapefile.
-	SF_OK: the file is a valid ESRI shapefile.
+    SF_ERROR: the file was not a valid ESRI shapefile.
+    SF_OK: the file is a valid ESRI shapefile.
 */
 SFResult validate_shapefile(void)
 {
     SFMainFileHeader header;
-	int32_t record_length;
+    int32_t record_length;
     
     fread(&header, sizeof(SFMainFileHeader), 1, g_shapefile);
     
@@ -144,46 +144,46 @@ SFResult validate_shapefile(void)
     
     g_shape_type = header.shape_type;
     g_shapefile_length = byteswap32(header.file_length);
-	record_length = byteswap32(header.file_length) - sizeof(SFMainFileHeader);
+    record_length = byteswap32(header.file_length) - sizeof(SFMainFileHeader);
 
-	switch ( g_shape_type ) {
-		case stNull:
-		case stPoint:
-		case stPolyline:
-			print_msg("Shape type <%d> not implemented.\n", g_shape_type);
-			break;
+    switch ( g_shape_type ) {
+        case stNull:
+        case stPoint:
+        case stPolyline:
+            print_msg("Shape type <%d> not implemented.\n", g_shape_type);
+            break;
         case stPolygon:
             g_polygons[g_polygons_index] = read_polygon();
             g_polygons_index++;
             break;
-		case stMultiPoint:
-		case stPointZ:
-		case stPolyLineZ:
-		case stPolygonZ:
-		case stMultiPointZ:
-		case stPointM:
-		case stPolyLineM:
-		case stPolygonM:
-		case stMultiPointM:
-		case stMultiPatch:
+        case stMultiPoint:
+        case stPointZ:
+        case stPolyLineZ:
+        case stPolygonZ:
+        case stMultiPointZ:
+        case stPointM:
+        case stPolyLineM:
+        case stPolygonM:
+        case stMultiPointM:
+        case stMultiPatch:
         default:
             print_msg("Encountered unsupported shape type <%d>\n", g_shape_type);
-			return SF_ERROR;
-	}
+            return SF_ERROR;
+    }
     
     return SF_OK;
 }
 
-/*	SFResult read_shapes(void)
+/*    SFResult read_shapes(void)
 
 Reads shapes from a shapefile.
 
 Arguments:
-	N/A.
-	
+    N/A.
+    
 Returns:
-	SF_ERROR: an error was encountered during reading of shapefiles.
-	SF_OK: no errors occurred.
+    SF_ERROR: an error was encountered during reading of shapefiles.
+    SF_OK: no errors occurred.
 */
 SFResult read_shapes(void)
 {
@@ -206,26 +206,26 @@ SFResult read_shapes(void)
 #endif
 
         switch ( shape_type ) {
-			case stNull:
-			case stPoint:
-			case stPolyline:
-				print_msg("Shape type <%d> not implemented.\n", shape_type);
-				fseek(g_shapefile, byteswap32(header.content_length) * sizeof(int16_t), SEEK_CUR);
-				break;
+            case stNull:
+            case stPoint:
+            case stPolyline:
+                print_msg("Shape type <%d> not implemented.\n", shape_type);
+                fseek(g_shapefile, byteswap32(header.content_length) * sizeof(int16_t), SEEK_CUR);
+                break;
             case stPolygon:
                 g_polygons[g_polygons_index] = read_polygon();
                 g_polygons_index++;
                 break;
-			case stMultiPoint:
-			case stPointZ:
-			case stPolyLineZ:
-			case stPolygonZ:
-			case stMultiPointZ:
-			case stPointM:
-			case stPolyLineM:
-			case stPolygonM:
-			case stMultiPointM:
-			case stMultiPatch:
+            case stMultiPoint:
+            case stPointZ:
+            case stPolyLineZ:
+            case stPolygonZ:
+            case stMultiPointZ:
+            case stPointM:
+            case stPolyLineM:
+            case stPolygonM:
+            case stMultiPointM:
+            case stMultiPatch:
             default:
                 print_msg("Encountered unsupported shape type <%d>\n", shape_type);
                 fflush(stdout);
@@ -237,22 +237,22 @@ SFResult read_shapes(void)
     return SF_OK;
 }
 
-/*	SFResult read_polygon(void)
+/*    SFResult read_polygon(void)
 
 Reads a polygon from a shapefile.
 
 Arguments:
-	N/A.
-	
+    N/A.
+    
 Returns:
-	SFPolygon: the polygon read from the shapefile.
+    SFPolygon: the polygon read from the shapefile.
 */
 SFPolygon read_polygon(void)
 {
     double box[4];
     int32_t num_parts = 0;
     int32_t num_points = 0;
-	SFPolygon polygon;
+    SFPolygon polygon;
     
     fread(box, sizeof(double) * 4, 1, g_shapefile);
     fread(&num_parts, sizeof(int32_t), 1, g_shapefile);
@@ -274,15 +274,15 @@ SFPolygon read_polygon(void)
     return polygon;
 }
 
-/*	SFResult close_shapefile(void)
+/*    SFResult close_shapefile(void)
 
 Closes the shapefile and frees all memory associated with it.
 
 Arguments:
-	N/A.
-	
+    N/A.
+    
 Returns:
-	SF_OK: no errors occurred.
+    SF_OK: no errors occurred.
 */
 SFResult close_shapefile(void)
 {
@@ -291,7 +291,7 @@ SFResult close_shapefile(void)
         g_shapefile = NULL;
     }
     if ( g_polygons != NULL ) {
-		int32_t x = 0;
+        int32_t x = 0;
         for ( ; x < g_polygons_index; ++x ) {
             SFPolygon polygon = g_polygons[x];
             if ( polygon.parts != NULL ) {
